257a3460f6ef64454674b633ff5c76e5ef048762
Your Name <you@example.com>
2024-11-30 22:38:42 +0530

    react notes

---
+++ app.js
@@ -0,0 +1,176 @@
+/*let arr=[10,20,30,40] ;
+const result=arr.map((element)=>{return(element+10)});
+console.log(result);
+
+const age=[10,20,24,50,35,43,56,67,80];
+const result=age.filter(checkAdult);
+
+console.log(result);
+function checkAdult(age)
+{
+    return age>=18;
+}
+    */
+/*
+const arr=[10,20,30,40,50,60];
+var result=arr.reduce(myFunc);
+console.log(result);
+function myFunc(num)
+{
+    return num;
+}
+    */
+   /*
+let num = 1;
+function printNumbers() {
+  if (num <= 5) {
+    console.log(num);
+    num += 1;
+    setTimeout(printNumbers, 1000); // Recursively call with 1-second delay
+  }
+}
+
+printNumbers();
+*/
+/*
+const numbers=[10,20,30,40,50];
+const result=numbers.reduce(myFunc);
+console.log(result);
+
+function myFunc(total,num)
+{
+  return total+num;
+}
+  */
+ /*
+ const array=[10,20,30,40,50];
+ console.log(array.splice(2));
+ console.log(array)
+ let arr=[10,20,30,40,50];
+ console.log(arr.slice(2));
+ console.log(arr)
+ */
+/*
+let list = [4, 5, 6];
+for (let i in list) 
+  {
+console.log(i);
+  }
+*/
+
+/*
+const arr=[1,2,3,4,5];
+const arr1=[11,2,22,33,44,55];
+
+console.log(...arr1,...arr);
+*/
+/*
+for(var i=0;i<=10;i++)
+  {
+      console.log(i)//output:0 1 2 3 4 5 6 7 8 9 10
+  }
+  console.log(i)//output:11
+ *//*
+  var a =200
+const x = a;
+console.log(x);//output:200
+
+a = 900;
+console.log(a);//
+
+const o = {a:300,b:400};
+o={k:"manjula"};
+o.a = 666;
+console.log(o);//
+
+*/
+
+/*
+var arr = [78,89,90,67]
+console.log(arr);//output:[ 78, 89, 90, 67 ]
+console.log(...arr);//output:78 89 90 67
+var arr2 = [56,89,45,34,arr]
+console.log(arr2);
+var arr3 = [56,89,45,34,...arr]
+        console.log(arr3);
+        var arr3 = [56,89,45,34,...arr]
+        console.log(arr3);
+
+        var arr4 = [56,89,45,34,...arr]
+        var s1 = "javascript";
+        console.log(...s1);//output:j a v a s c r i p t//it separetes elements
+        console.log(s1);//output:javascript
+
+
+*/
+
+
+
+/*
+var arr8 = [1,2,3,4,5]
+var arr9 = [6,7,8,9]
+var arr10 = [...arr8,...arr9]
+console.log(arr10);
+
+var fruits=["apple","banana","grapes","sapota"];
+
+for(x of fruits)
+{
+  console.log(x);
+}
+for(index in fruits)
+{
+  console.log(index+":"+fruits[index]);
+}
+var students = {'100':"xyz",'1002':"pqr",'103':"mno"}
+for(key in students){
+    console.log(key+":"+students[key]+"<br>")
+}
+    
+*/
+/*
+var counter = 1;
+        var x= setInterval(test,3000,"xyz");//test is called call back function
+        console.log(x);
+        function test(name){
+            console.log("called :" +counter);
+            counter++;
+        }//
+
+
+*//*
+var x = setTimeout(function(name){
+  console.log(x+name);//output:1
+},3000,"manjula");
+console.log(x);//output:1manjula
+
+
+
+*/
+const i=10;
+console.log(i);
+i=20;
+console.log(i);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+  
